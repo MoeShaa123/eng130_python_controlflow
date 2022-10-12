7ed0d3e8c2dd5c83ba5fe15bcf1c5beec0a46343
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-12 14:23:08 +0100

    first commit

--- control_flow.py
+++ control_flow.py
@@ -1,16 +1,9 @@
-# Python Control Flow
-## if elif else
-### Loops - for loop - while loop
-#### Python keywords break - continue etc.
-# if it's cold:
-    take jacket
-# if it's raining:
-    rain mack
-# if it's sunny: boolean value true - next line
-    lets go to the beach
-# else or elif
 
-```python
+
+
+
+
+
 weather = "cold"
 
 # if it's cold:
@@ -27,5 +20,10 @@ elif weather == "sunny":
 else:
 	print("bring raincoat")
 
-```
 
+age = 18
+
+if age >= 18:
+    print("You can buy a ticket")
+elif age >= 16:
+    print("")
